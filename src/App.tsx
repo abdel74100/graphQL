6bc3eb1ce04f3903d61faf86690a520871e5251f
abdel <a.akroh@e-durable.ch>
2020-12-26 01:24:39 +0100

    build app pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from "@apollo/react-hooks";
+import { PokemonsContainer } from '../src/components/PokemonsContainer';
 import './App.css';
-import { useQuery, gql, ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-
-import { Apollo } from './components/apollo';
-
-const client = new ApolloClient({
-    uri: 'https://api.spacex.land/graphql/',
-    cache: new InMemoryCache()
-});
-
-client
-    .query({
-        query: gql`
-            {
-                launches(limit: 5) {
-                    launch_date_utc
-                    launch_success
-                    rocket {
-                        rocket_name
-                    }
-                    links {
-                        video_link
-                    }
-                    details
-                }
-            }
-        `
-    })
-    .then(result => console.log(result));
 
 function App() {
+    const client = new ApolloClient({
+        uri: 'https://graphql-pokemon2.vercel.app/'
+    });
+
     return (
         <ApolloProvider client={client}>
-            <div className="example-default">
-                <Apollo />
-            </div>
-        </ApolloProvider >
+        <PokemonsContainer />
+        </ApolloProvider>
     );
 }
 
